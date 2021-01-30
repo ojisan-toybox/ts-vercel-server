15ff3e3c95dd729a507bdb46d0a190dbe4e04384
sadness_ojisan <sadness.ojisan@gmail.com>
2021-01-31 05:36:04 +0900

    add ts endpoint

---
+++ api/sample.ts
@@ -0,0 +1,22 @@
+import { NowRequest, NowResponse, VercelRequestQuery } from "@vercel/node";
+
+type QueryParams = {
+  id: string;
+};
+
+export default async (req: NowRequest, res: NowResponse) => {
+  const { query } = req;
+  if (!_isValidVercelRequest(query)) {
+    throw new Error("invalid request");
+  }
+  const { id } = query;
+  return res.json({ id });
+};
+
+const _isValidVercelRequest = (query: any): query is QueryParams => {
+  if (typeof query.id !== "string") {
+    console.error("invalid id type.");
+    return false;
+  }
+  return true;
+};
